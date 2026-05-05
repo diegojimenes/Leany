6bef63885c49a7649edaacefb7f3e111150e9af9
diego jimenes <lligaskate@gmail.com>
2026-05-05 09:03:22 -0300

    feat: setup migration environment and database config

---
+++ typeorm.config.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'leany',
+  entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/src/database/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
